f414542bfb1d55c2a575d7efdee0ed6cc553eb39
Delight Sheriff <delightsheriff@gmail.com>
2024-02-21 20:34:42 +0100

    2. Accessing nested elements:2

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,3 +1,7 @@
+import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-  return object.getIn(array);
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
 }
