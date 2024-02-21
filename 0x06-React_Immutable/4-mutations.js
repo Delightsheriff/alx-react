b5106170c41c162a7b965d8574b2d03c0cb88246
Delight Sheriff <delightsheriff@gmail.com>
2024-02-21 21:00:33 +0100

    4. Chained mutations:4

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export const map = new Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,4 +9,6 @@ export const map = new Map({
   6: 'Lucas',
 });
 
-export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+export const map2 = map.withMutations((values) => {
+  values.set(2, 'Benjamin').set(4, 'Oliver');
+});
